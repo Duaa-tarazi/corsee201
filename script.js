173f9c1badbab121f6cb1deff1bfbeb2b2d747c7
duaa-tarazi <dst08itm@yahoo.com>
2021-05-23 17:14:10 +0300

    edit coursee201' duaa-tarazi
    clear

---
+++ script.js
@@ -0,0 +1,12 @@
+let firstname = prompt("add your firstname ");
+console.log(firstname); 
+let  lastname= prompt("add your lastname ");
+let age = prompt("add your age ");
+let duaa = prompt("add your name ");
+
+
+console.log(lastname); 
+console.log(age) ;
+console.log(duaa); 
+
+alert('your name:' +firstname+lastname+'your age:'+age)
